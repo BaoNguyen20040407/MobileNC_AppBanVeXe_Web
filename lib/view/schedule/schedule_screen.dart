05f1ff08a5fbbfe0a7e06c8a1a1fe820fac2dfcb
BaoNguyen20040407 <baonguyenhuflit@gmail.com>
2025-06-21 14:35:21 +0700

    Tinh chỉnh khung tìm kiếm

--- lib/view/schedule/schedule_screen.dart
+++ lib/view/schedule/schedule_screen.dart
@@ -112,7 +112,7 @@ class _ScheduleScreenState extends State<ScheduleScreen> {
       const SizedBox(height: 20),
       Container(
         margin: const EdgeInsets.symmetric(horizontal: 24),
-        padding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
+        padding: const EdgeInsets.fromLTRB(12, 0, 12, 12),
         decoration: BoxDecoration(
           color: Colors.white,
           borderRadius: BorderRadius.circular(12),

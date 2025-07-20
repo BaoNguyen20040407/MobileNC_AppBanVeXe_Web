6c1df0f8dc96720eff079cf2b3e5216a8ab2c6d1
BaoNguyen20040407 <baonguyenhuflit@gmail.com>
2025-07-20 09:03:10 +0700

    Bảo - Tinh chỉnh giao diện

--- lib/view/admin/support_admin/support_list.dart
+++ lib/view/admin/support_admin/support_list.dart
@@ -5,6 +5,7 @@ import 'package:giao_dien_1/widget/exit_button.dart';
 import 'package:giao_dien_1/widget/choice_chip_selector.dart';
 import 'package:giao_dien_1/view/admin/home_admin/homeadmin.dart';
 import 'package:giao_dien_1/view/admin/support_admin/reply_support.dart';
+import 'package:giao_dien_1/widget/pagination_control.dart';
 
 class SupportListScreen extends StatefulWidget {
   const SupportListScreen({super.key});
@@ -242,25 +243,14 @@ class _SupportListScreenState extends State<SupportListScreen> {
                       const SizedBox(height: 16),
 
                       // Phân trang (tạm để giống)
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          TextButton(
-                              onPressed: () {},
-                              child: const Text("Đầu", style: TextStyle(fontFamily: 'Inter', color: AppColors.black))),
-                          Container(
-                            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-                            margin: const EdgeInsets.symmetric(horizontal: 4),
-                            decoration: BoxDecoration(
-                              color: AppColors.mainOrange,
-                              borderRadius: BorderRadius.circular(4),
-                            ),
-                            child: const Text('1', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
-                          ),
-                          TextButton(
-                              onPressed: () {},
-                              child: const Text("Cuối", style: TextStyle(fontFamily: 'Inter', color: AppColors.black))),
-                        ],
+                      PaginationControls(
+                        currentPage: 1,
+                        onFirstPressed: () {
+                          print("Go to first page");
+                        },
+                        onLastPressed: () {
+                          print("Go to last page");
+                        },
                       ),
                     ],
                   ),
